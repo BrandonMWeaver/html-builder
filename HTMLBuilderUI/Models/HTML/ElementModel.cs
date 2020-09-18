44b7d2a39447a34f1737e55602d706dbb9474d1d
Brandon Weaver <a7xncob@gmail.com>
2020-09-18 19:57:20 -0400

    Add element and template models.

---
+++ HTMLBuilderUI/Models/HTML/ElementModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLBuilderUI.HTML.Models
+{
+    class ElementModel
+    {
+        public string Type { get; set; }
+        public string InnerHTML { get; set; }
+
+        public ElementModel(string type, string innerHTML = "")
+        {
+            this.Type = type;
+            this.InnerHTML = innerHTML;
+        }
+
+        public override string ToString()
+        {
+            return $"<{this.Type}>{InnerHTML}</{Type}>";
+        }
+    }
+}
---
+++ HTMLBuilderUI/Models/HTML/TemplateModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HTMLBuilderUI.HTML.Models
+{
+    class TemplateModel
+    {
+        public List<ElementModel> Elements { get; set; }
+
+        public override string ToString()
+        {
+            string body = "";
+            foreach(ElementModel element in this.Elements)
+            {
+                body = $"{body}{element}";
+            }
+            return $"<!DOCTYPE html><html><head><title>Test</title></head><body>{body}</body></html>";
+        }
+    }
+}
